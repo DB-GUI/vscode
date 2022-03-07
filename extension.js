eee86d39d9bc31155058700a7159025fff1ef215
关于 <372301467@qq.com>
2022-03-07 10:24:40 +0800

    init

--- extension.js
+++ extension.js
@@ -1,36 +1,12 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require('vscode')
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "dbms-gui" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('dbms-gui.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from DBMS-GUI!');
-	});
-
-	context.subscriptions.push(disposable);
+/** @param {vscode.ExtensionContext} context */
+exports.activate = function(context) {
+  console.debug('dbms-gui activating')
+  
+  console.debug('dbms-gui activated')
 }
 
-// this method is called when your extension is deactivated
-function deactivate() {}
-
-module.exports = {
-	activate,
-	deactivate
+exports.deactivate = function() {
+  console.debug('dbms-gui deactivated')
 }

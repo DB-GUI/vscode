c3a23d490f653bd0f51fcbe961bfeb162fbd6b70
关于 <372301467@qq.com>
2022-03-08 09:51:35 +0800

    command addConnection

--- extension.js
+++ extension.js
@@ -3,7 +3,11 @@ const vscode = require('vscode')
 /** @param {vscode.ExtensionContext} context */
 exports.activate = function(context) {
   console.debug('dbms-gui activating')
-  
+
+	context.subscriptions.push(vscode.commands.registerCommand('ppz.addConnection', () => {
+		vscode.window.showInformationMessage('Hello World!')
+	}))
+
   console.debug('dbms-gui activated')
 }
 

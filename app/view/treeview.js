e5f45f8d4ef1433726af2a6a0149da837c0b1e57
关于 <372301467@qq.com>
2022-03-25 17:15:19 +0800

    treeview 合并 getChildre 和 getItem

--- app/view/treeview.js
+++ app/view/treeview.js
@@ -9,21 +9,25 @@ module.exports = function() {
   })
 }
 
-function El(type, data) {
-  return { type, data }
-}
+const El = (type, data, treeItem) => ({ type, data, treeItem })
 async function getChildren(el) {
   try {
     if(!el) // root
       return connectionService.getAllData()
-        .map(data => El('connection', data))
+        .map(data => El('connection', data, {
+          label: data.name || '未命名连接',
+          icon: `dbms/${data.client}.svg`
+        }))
     switch(el.type) {
       case 'connection':
         try {
           if(!el.connection)
             el.connection = connectionService.connect(el.data)
           const dbList = await el.connection.dbList()
-          return dbList.map(name => El('database', { name }))
+          return dbList.map(name => El('database', { name }, {
+            label: name,
+            icon: 'database.svg'
+          }))
         } catch(err) {
           const msg = '连接建立失败，请检查连接信息和数据库服务器的设置'
           console.error(msg, el.data, err)
@@ -46,36 +50,11 @@ async function getChildren(el) {
   }
 }
 
-async function getTreeItem(el) {
-  switch(el.type) {
-    case 'connection':
-      return new ConnectionTreeItem(el.data)
-    case 'database':
-      return new DatabaseTreeItem(el.data.name)
-    default:
-      throw Error('意外的结点类型', el)
-  }
-}
-
-function localPath(target) {
-  return Path.join(__filename, '../../../assets/icon', target)
-}
-class TreeItem extends vscode.TreeItem {
-  constructor(label, collapsibleState = vscode.TreeItemCollapsibleState.Collapsed) {
-    super(label, collapsibleState)
-  }
-}
-
-class ConnectionTreeItem extends TreeItem {
-  constructor({ name, client }) {
-    super(name || '未命名连接')
-    this.iconPath = localPath(`dbms/${client}.svg`)
-  }
-}
-class DatabaseTreeItem extends TreeItem {
-  constructor(label) {
-    super(label)
-    
-    this.iconPath = localPath('database.svg')
-  }
+async function getTreeItem({ treeItem: {
+  label, icon, collapsibleState = vscode.TreeItemCollapsibleState.Collapsed
+}}) {
+  const result = new vscode.TreeItem(label, collapsibleState)
+  if(icon)
+    result.iconPath = Path.join(__filename, '../../../assets/icon', icon)
+  return result
 }

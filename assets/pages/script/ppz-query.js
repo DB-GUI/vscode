8cbf292f179ba0b386e1663f9f8b804e436ad272
关于 <372301467@qq.com>
2022-03-30 11:01:13 +0800

    TableWebview $$params.$$fields

--- assets/pages/script/ppz-query.js
+++ assets/pages/script/ppz-query.js
@@ -1,7 +1,18 @@
+/** @returns {HTMLElement} */
 export default function $(selector) {
   if(typeof selector == 'string')
     return document.querySelector(selector)
   return selector
+}
+
+$.El = function(tagname, className, children) {
+  /** @type {HTMLElement} */
+  const $el = document.createElement(tagname)
+  if(className)
+    $el.className = className
+  if(children)
+    $el.append(...children)
+  return $el
 }
 
 $.clone = function(data) {
@@ -9,6 +20,16 @@ $.clone = function(data) {
   for(let k in data)
     result[k] = data[k]
   return result
+}
+
+// 可优化
+$.msg = function(messageType, handler) {
+  window.addEventListener('message', function({ data: { type, data }}) {
+    if(messageType == type) {
+      console.debug('收到消息', type, data)
+      handler(data)
+    }
+  })
 }
 
 $.isNil = function(target) {
--- assets/pages/table/index.js
+++ assets/pages/table/index.js
@@ -1,3 +1,22 @@
-const vscode = acquireVsCodeApi()
+import $ from '../script/ppz-query.js'
+const $body = $('body')
 
-window.addEventListener('message', console.log)
+var $$params = new function() {
+  const $params = $.El('div', 'params')
+  var $$fields = new function() {
+    let $fields = $.El('div', 'fields') // fields 容器
+    const Field = function() {
+      function Field(options) {
+        this.$el = $.El('div', 'item', [options.name])
+        this.$el.title = options.name + ': ' + options.type
+      }
+      return Field
+    }()
+    $.msg('fields', fields => {
+      this.items = fields.map(f => new Field(f))
+      $fields.replaceChildren(...this.items.map(item => item.$el))
+    })
+    $params.appendChild($fields)
+  }
+  $body.appendChild($params)
+}

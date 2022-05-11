8c3784752b255989ac1e7e5fb9875b650e323b4d
关于 <372301467@qq.com>
2022-05-11 11:47:14 +0800

    table 页，完善保存按钮、翻页组件逻辑

--- assets/webview/pages/table/index.js
+++ assets/webview/pages/table/index.js
@@ -35,7 +35,7 @@ new Page({
     const header = new function() {
       const pagination = new function() {
         const { count, index, size } = page.state.selectParams.pagination
-        return Pagination({
+        const p = Pagination({
           count, index, size,
           onChange({ index, size }) {
             page.state.selectParams.pagination = { index, size }
@@ -43,6 +43,20 @@ new Page({
             refreshData()
           }
         })
+        const isDisabledBtn = btn => btn.tagName == 'BUTTON' && btn.disabled
+        p.$el.addEventListener('click', function(evt) {
+          const name = evt.target.tagName
+          if(!['BUTTON', 'svg', 'use'].includes(name)) return // 不是按钮的不处理
+          if(isDisabledBtn(evt.target) // 是按钮但 disabled 的也不处理
+            || isDisabledBtn(evt.target.parentElement)
+            || isDisabledBtn(evt.target.parentElement.parentElement)) return
+          
+          if(table.isEditing()) {
+            $.noty.warn('请先保存或撤销全部修改')
+            evt.stopPropagation()
+          }
+        }, true)
+        return p
       }
 
       this.$el = $.El('header', '', [
@@ -56,10 +70,11 @@ new Page({
         $.Div('operations', [
           $.Div('btns', [
             Button('刷新', 'light', function() {
-              if(table.isEditing())
+              if(table.isEditing()) {
                 $.noty.warn('请先保存或撤销全部修改')
-              else
-                refreshData()
+                return
+              }
+              refreshData()
             }),
             Button('字段选择', 'filter', function() {
             }),
@@ -68,6 +83,10 @@ new Page({
             Button('拷贝当前记录', 'copy', function() {
             }),
             Button('保存', 'save', function() {
+              if(!table.isEditing()) {
+                $.noty.warn('没有待保存的数据')
+                return
+              }
               console.log(state.table)
             }),
             Button('撤销全部', 'undo', function() {

eb43e5c76821d3b79046f301d0a41153f4d33afa
关于 <372301467@qq.com>
2022-06-03 22:54:15 +0800

    更新图标 命令：github

--- app/command/other.js
+++ app/command/other.js
@@ -1,5 +1,6 @@
 const noty = require('../../lib/vscode-utils/noty')
 const warn = require('../../lib/vscode-utils/prompt/confirm').warn
+const cp = require('child_process')
 
 exports.empty = async function() {
   if(await warn('确认清除所有数据吗？', '此操作将清除全部 ppz 数据', '确认清除'))
@@ -11,4 +12,8 @@ exports.empty = async function() {
     )
   )
   noty.info('数据已清空，请重启 vscode')
+}
+
+exports.github = async function() {
+  cp.exec('start https://github.com/ppz-pro/ppz.vscode')
 }

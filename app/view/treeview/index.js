31a110ac8f8761296c0eda1c69aca3883cce368d
关于 <372301467@qq.com>
2022-06-03 23:42:40 +0800

    帮助与反馈模块

--- app/view/treeview/index.js
+++ app/view/treeview/index.js
@@ -1,6 +1,8 @@
 const vscode = require('vscode')
 const connection = require('./connection')
+const help = require('./help')
 
 module.exports = function() {
   vscode.window.createTreeView('connection', connection.treeviewOptions)
+  vscode.window.createTreeView('help', help)
 }

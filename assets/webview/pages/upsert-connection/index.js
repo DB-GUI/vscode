78b9e6c1598a3133fa34d2d4a003cd22103336dd
关于 <372301467@qq.com>
2022-06-20 16:28:42 +0800

    优化 vue-page 页面逻辑

--- assets/webview/pages/upsert-connection/index.js
+++ assets/webview/pages/upsert-connection/index.js
@@ -1,76 +1,33 @@
-import { Div } from '../../../../lib/dom/index.js'
-import Page from '../../script/vue/page.js'
+import VuePage from '../../script/vue/page.js'
 import FormOptions from './form-options.js'
 
-new class extends Page {
-  initVue(page) {
-    if(!page.state) {
-      page.state = FormOptions(PPZ.initData.editing)
-      page.saveState() // 这样虽然繁琐，但省掉了“理解”成本
-    }
-
-    return [
-      Div('flex-container'),
-      {
-        data() {
-          return page.state
-        },
-        template: `
-          <div class="form-container">
-            <div class="client-selector">
-              <button
-                v-for="form in forms"
-                :class="{ selected: current == form.key }"
-                @click="select(form.key)"
-              >{{form.label}}</button>
-            </div>
-            <div class="forms">
-              <div class="public form">
-                <label>
-                  <span>name</span>
-                  <input v-model="publicForm.name" />
-                </label>
-              </div>
-              <template v-for="form in forms">
-                <div class="private form" v-show="form.key == current">
-                  <label v-for="field in form.fields">
-                    <span>{{field.name}}</span>
-                    <file-input v-if="field.type == 'file'" v-model="field.value" />
-                    <input v-else v-model="field.value" />
-                  </label>
-                </div>
-              </template>
-            </div>
-          </div>
-          <div class="form-btns">
-            <button @click="save(true)">保存并连接</button>
-            <button @click="save()">保存</button>
-          </div>
-        `,
-        methods: {
-          select(key) {
-            this.current = key
-          },
-          async save(connect) {
-            await page.api.save({
-              connect,
-              record: this.getFormData()
-            })
-          },
-          getFormData() {
-            const form = this.forms.find(form => form.key == this.current)
-            const result = {
-              client: form.key,
-              name: this.publicForm.name
-            }
-            if(PPZ.initData.editing)
-              result.id = PPZ.initData.editing.id
-            for(const field of form.fields)
-              result[field.name] = field.value
-            return result
-          }
+VuePage(function(page) {
+  return {
+    initData() {
+      return FormOptions(PPZ.initData.editing)
+    },
+    methods: {
+      select(key) {
+        this.current = key
+      },
+      async save(connect) {
+        await page.api.save({
+          connect,
+          record: this.getFormData()
+        })
+      },
+      getFormData() {
+        const form = this.forms.find(form => form.key == this.current)
+        const result = {
+          client: form.key,
+          name: this.publicForm.name
         }
+        if(PPZ.initData.editing)
+          result.id = PPZ.initData.editing.id
+        for(const field of form.fields)
+          result[field.name] = field.value
+        return result
       }
-    ]
+    }
   }
-}
+})
--- assets/webview/script/vue/page.js
+++ assets/webview/script/vue/page.js
@@ -1,20 +1,27 @@
-import { $ } from '../../../../lib/dom/index.js'
 import Page from '../page.js'
 import { createApp } from './vue.esm-browser.prod.js'
 import FileInput from './file-input.js'
 
 export default
-class VuePage extends Page {
-  init(page) {
-    const [$mounter, options] = this.initVue(page)
-
-    options.updated = function() {
-      page.saveState()
+function VuePage(getVueOptions) {
+  new class extends Page {
+    init(page) {
+      const options = getVueOptions(page)
+      // state 初始化
+      if(!page.state) {
+        page.state = options.initData()
+        page.saveState()
+      }
+      delete options.initData
+      options.data = () => page.state
+      // updateState
+      options.updated = function() {
+        page.saveState()
+      }
+      
+      const app = createApp(options)
+      app.component('file-input', FileInput)
+      app.mount('#vue-app')
     }
-    const app = createApp(options)
-    app.component('file-input', FileInput)
-    app.mount($mounter)
-
-    $('body').append($mounter)
   }
 }

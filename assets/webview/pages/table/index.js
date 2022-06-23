5296a2f6f97c2dc2f6a85a792bba277204be2268
pp <18292052057@163.com>
2022-06-23 21:43:47 +0800

    webview table 删除记录

--- assets/webview/pages/table/index.js
+++ assets/webview/pages/table/index.js
@@ -39,28 +39,55 @@ VuePage(function(page) {
         const data = {
           fields: this.fields
         }
-        if(copy) {
-          if(this.pneOptions.focus.y !== undefined)
-            data.record = this.records[this.pneOptions.focus.y]
-          else {
-            page.noty.warn('请先点击想要拷贝的记录')
-            return
-          }
-        }
+        if(copy)
+          data.record = this.focusedRecord
         page.api.newRecord(data)
+      },
+      drop() {
+        const warning = this.pkNames
+          .map(name => `${name} 为 ${this.focusedRecord[name]}`)
+          .join(' 且 ')
+        page.prompt.warn(
+          '确定删除？',
+          '您正在删除 ' + warning + ' 的记录，删除后不可恢复', 
+          {
+            确定: async () => {
+              const success = await page.api.drop(this.focusedPKValues)
+              if(success)
+                this.putData()
+            }
+          }
+        )
       }
     },
     computed: {
+      pks() { return this.fields.filter(f => f.pk) },
+      pkNames() { return this.pks.map(f => f.name) },
       hasPK() {
         if(this.fields.length == 0)
-          return null
-        return this.fields.some(f => f.pk)
+          return undefined
+        return this.pks.length > 0
+      },
+      focusedRecord() {
+        if(this.pneOptions.focus.y !== undefined)
+          return this.records[this.pneOptions.focus.y]
+        return null
+      },
+      focusedPKValues() {
+        if(this.pkNames.length == 0)
+          throw Error('无主键')
+        const result = {}
+        for(const name of this.pkNames)
+          result[name] = this.focusedRecord[name]
+        return result
       }
     },
     watch: {
-      hasPK(nv) {
-        if(nv === false)
+      hasPK(nv, ov) {
+        if(!nv)
           page.noty.warn(tableName + ' 表缺少主键，不能进行编辑、删除操作')
+        else if(ov === false) // 现在有，且刚才没有
+          page.noty.info(tableName + ' 表已添加主键')
       }
     },
     mounted() {

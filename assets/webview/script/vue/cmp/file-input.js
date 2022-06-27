f48a932472a92dcfa7acde8cdda9d2f8d189a7ed
关于 <372301467@qq.com>
2022-06-27 14:46:12 +0800

    webview：调整 form 样式 webview：调整 file-input 逻辑

--- assets/webview/script/vue/cmp/file-input.js
+++ assets/webview/script/vue/cmp/file-input.js
@@ -1,44 +1,17 @@
 import selectFile from '../../../../../lib/vscode-utils/file-selector/client.js'
-import { ref, watch } from '../vue.esm-browser.prod.js'
 
 export const name = 'file-input'
 
 export const options = {
-  template: `
-    <span class="file-input">
-      <ppz-input v-model="value" @focus="select">
-      <button @click="select">···</button>
-    </span>
-  `,
   props: ['modelValue'],
-  setup(props, ctx) {
-    const value = ref(props.modelValue)
-    watch(props.modelValue, nv => value.value = nv)
-    watch(value, nv => {
-      ctx.emit('update:modelValue', nv)
-    })
-
-    return {
-      value,
-      async select() {
-        const file = await selectFile()
-        if(!file) return
-        const path = file[0].path
-        value.value = path
-      }
+  template: `
+    <ppz-input v-model="modelValue" @focus="select" />
+  `,
+  methods: {
+    async select() {
+      const file = await selectFile()
+      if(!file) return
+      this.$emit('update:modelValue', file[0].path)
     }
   }
 }
-
-export const style = `
-  .file-input > input {
-    width: 8rem;
-    margin-left: 1rem;
-    margin-right: .36rem;
-  }
-  .file-input > button {
-    padding: 0;
-    width: 2em;
-    min-width: 2em;
-  }
-`

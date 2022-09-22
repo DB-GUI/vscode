cd83e1d034ca8291b4584a339e3b202e32a0a027
关于 <372301467@qq.com>
2022-09-22 23:46:08 +0800

    style: 0.4.0 -> 0.5.0

--- app/model/migration/0-5-0.js
+++ app/model/migration/0-5-0.js
@@ -1,15 +1,15 @@
 module.exports = async function(state) {
   const { version } = state.get('system')
   if(version === '0.0.0') {
-    console.log('migrating from 0.0.0 to 0.4.0')
+    console.log('migrating from 0.0.0 to 0.5.0')
     // 更新版本号
     await state.update('system', {
-      version: '0.4.0'
+      version: '0.5.0'
     })
 
     // 数据存储切换到 @ppzp/bd
     // 原主键名 id 改为 _id
-    const connections = state.get('connection')
+    const connections = state.get('connection', [])
     for(let conn of connections) {
       conn._id = conn.id
       delete conn.id

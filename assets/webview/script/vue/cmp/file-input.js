7e0dee1f6777a7308cceb2aa087b03561ff23b26
关于 <372301467@qq.com>
2022-09-28 15:09:20 +0800

    fix: issue#34 sqlite创建连接时，弹出窗口不能正常关闭

--- assets/webview/script/vue/cmp/file-input.js
+++ assets/webview/script/vue/cmp/file-input.js
@@ -2,23 +2,14 @@ import selectFile from '../../../../../lib/vscode-utils/file-selector/client.js'
 
 export const name = 'file-input'
 
-var selecting = false
-
 export const options = {
   props: ['modelValue'],
   template: `
-    <ppz-input v-model="modelValue" @focus="select" />
+    <ppz-input v-model="modelValue" @click="select" />
   `,
   methods: {
     async select() {
-      if(selecting) {
-        console.error('already in selecting')
-        return
-      }
-      selecting = true
-      
       const file = await selectFile()
-      selecting = false
       if(!file) return
       this.$emit('update:modelValue', file[0].path)
     }

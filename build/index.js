2ca7397ca87cd370f638b49baed186c66a8601be
关于 <372301467@qq.com>
2022-09-29 16:38:16 +0800

    build: reorgnanize code

--- build/index.js
+++ build/index.js
@@ -3,17 +3,29 @@ const FS = require('node:fs/promises')
 const esbuild = require('esbuild')
 const packageJson = require('../package.json')
 
-;(async function package() {
-  // 打包类型
+;(async function() {
+  const buildType = getBuildType()
+
+  await deleteOldCreateNew()
+  await copyStatics()
+  await writePackageJSON()
+  await esbuildBuild(buildType)
+})()
+
+/** 检测 build type */
+function getBuildType() {
   const buildType = process.argv[2]
   if(buildType != 'dev' && buildType != 'pro')
     throw Error('error build type: ' + buildType)
   console.log('build type:', buildType)
+  return buildType
+}
 
-  // 删除旧目录
-  const dir = p('../dist')
+/** 删除旧的 dist，创建新的 dist */
+async function deleteOldCreateNew() {
+  const dir = p('../dist') // 目标
   try {
-    await FS.rm(dir, {
+    await FS.rm(dir, { // 删除旧的
       recursive: true,
       force: true
     })
@@ -21,9 +33,12 @@ const packageJson = require('../package.json')
   } catch(err) {
     console.log('未检测到老文件或...', err)
   }
-  await FS.mkdir(dir)
+  await FS.mkdir(dir) // 创建新的
   console.log('创建目录', dir)
+}
 
+/** 把静态文件直接复制过去 */
+async function copyStatics() {
   // webview
   await cp('assets')
   // lib
@@ -33,27 +48,24 @@ const packageJson = require('../package.json')
   await cp('LICENSE')
   await cp('README.md')
   console.log('copy assets, changelog, license, readme')
+}
 
-  // package.json
-  delete packageJson.devDependencies
-  delete packageJson.dependencies
+/** 处理 package.json */
+async function writePackageJSON() {
+  delete packageJson.devDependencies // 删除不需要的 dev dependencies 声明
+  delete packageJson.dependencies // 删除已打包的 dependencies
   await FS.writeFile(p('../dist/package.json'), JSON.stringify(packageJson))
   console.log('write package.json')
+}
 
-  // .ignore
-  await FS.writeFile(p('../dist/.vscodeignore'), `
-    *.vsix
-    *.js.map
-  `)
-  console.log('write .vscodeignore')
-
-  // 打包 extension.js
+/** 使用 esbuild 编译 */
+async function esbuildBuild(buildType) {
   await esbuild.build({
     watch: buildType == 'dev',
     minify: buildType == 'pro',
     entryPoints: [p('../app/extension.js')],
     bundle: true,
-    sourcemap: true,
+    sourcemap: buildType == 'dev',
     external: [
       'vscode',
       'sqlite3',
@@ -64,13 +76,14 @@ const packageJson = require('../package.json')
     outfile: p('../dist/extension.js'),
   })
   console.log('打包成功，非 app 目录的文件发生改动时，要重启此命令！！！')
-})()
+}
 
-// 工具
+// --------------------------------
+// 下面是一些工具
 function p(target) { // 获取绝对路径
   return Path.join(__dirname, target)
 }
-async function cp(from) { // 复制
+async function cp(from) { // 复制到 dist 文件夹
   const to = '../dist/' + from
   from = '../' + from
   await FS.cp(p(from), p(to), {
